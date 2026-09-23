9375401184819bb3fbf37901be89e82863d430fa
fanyongchao <fanyongchao.fyc@gmail.com>
2026-09-23 21:56:51 +0800

    perf: switch NutUI to on-demand styles

--- apps/weapp/src/app.tsx
+++ apps/weapp/src/app.tsx
@@ -1,7 +1,10 @@
 import { PropsWithChildren } from 'react'
 import { useLaunch } from '@tarojs/taro'
 import { I18nProvider } from './i18n'
-import '@nutui/nutui-react-taro/dist/style.css'
+import '@nutui/nutui-react-taro/dist/styles/themes/default.css'
+import '@nutui/nutui-react-taro/dist/es/packages/button/style/style.css'
+import '@nutui/nutui-react-taro/dist/es/packages/cell/style/style.css'
+import '@nutui/nutui-react-taro/dist/es/packages/cellgroup/style/style.css'
 import './app.scss'
 
 function App ({ children }: PropsWithChildren<any>) {
